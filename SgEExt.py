9c48fe4d544b4bb79864aac066dfab6360efcc33
Samuel FORESTIER <dev@samuel.domains>
2019-03-30 09:33:52 +0100

    Bumps version, tag on its way !

--- SgEExt.py
+++ SgEExt.py
@@ -276,7 +276,7 @@ def main():
     )
     parser.add_argument(
         '--version',
-        action='version', version="%(prog)s : 2.0"
+        action='version', version="%(prog)s : 2.1"
     )
 
     # Normalize the user-supplied target directory.
